26c1bb34318b194ab2ba97fb87d7cafa4ee65293
mabmab55 <bambam_lacerda001@hotmail.com>
2023-06-15 01:01:01 -0300

    JavaFX and Scene Builder packages

---
+++ src/app/TelaPrincipal.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package app;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Emerson
+ */
+public class TelaPrincipal extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader carregador = new FXMLLoader();
+        String tela = "./telas/TelaPrincipal.fxml";
+        AnchorPane root = (AnchorPane) carregador.load(getClass().getClassLoader().getResource(tela));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
