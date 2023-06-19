422f0619d4c563fa8762690e264abdd2d730bbf8
mabmab55 <bambam_lacerda001@hotmail.com>
2023-06-18 22:48:16 -0300

    Fixing MelClinic.java package and class names

--- src/melclinic/MelClinic.java
+++ src/melclinic/MelClinic.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package melpetshop;
+package melclinic;
 
 /**
  *
  * @author roberson
  */
-public class MelPetshop {
+public class MelClinic{
 
     /**
      * @param args the command line arguments
