9f801a9a5b066d8a5a1e57c2632e1b8e069d9294
mabmab55 <bambam_lacerda001@hotmail.com>
2023-06-22 16:22:58 -0300

    Grabbing the professionals from database and button to delete them on the interface

--- src/controlesTelas/TelaProfissionaisController.java
+++ src/controlesTelas/TelaProfissionaisController.java
@@ -6,14 +6,19 @@ package controlesTelas;
 
 import DAO.ProfissionalDAO;
 import entidades.Profissional;
+import helpers.GenericCreateTableButton;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -26,7 +31,7 @@ public class TelaProfissionaisController implements Initializable {
     private TableView<Profissional> TableViewProfissionais;
     @FXML
     private Button novoProfissional;
-    
+
     private ObservableList profissionais;
     final ProfissionalDAO profissionalDao = new ProfissionalDAO();
 
@@ -36,11 +41,63 @@ public class TelaProfissionaisController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        updateProfissionais();
+
+        setupTable();
+    }
+
+    private void updateProfissionais() {
+        try {
+            //findallpessoas kkkkkkkkkk
+            List<Profissional> results = profissionalDao.findAllPessoas();
+
+            if (profissionais == null) { //checar se pessoas eh nulo para inicializar a lista de pessoas
+                System.out.println("IF");
+                profissionais = FXCollections.observableArrayList(results);
+            } else { //caso contrario limpa para adicionar a nova lista atualizada
+                System.out.println("ELSE");
+                profissionais.clear();
+                profissionais.addAll(results);
+            }
+
+        } catch (Exception e) {
+        }
+    }
+
+    private TableColumn createTableColumn(String columnText, double minWidth, String propertyName) {
+        TableColumn column = new TableColumn<Profissional, Integer>();
+        column.setText(columnText);
+        column.setMinWidth(minWidth);
+        column.setCellValueFactory(new PropertyValueFactory(propertyName));
+
+        return column;
+    }
+
+    private TableColumn<Profissional, Void> createProfissionalButtonColumn() {
+        return GenericCreateTableButton.createButtonColumn(profissional -> {
+            try {
+                profissionalDao.delete(profissional.getId());
+                updateProfissionais();
+            } catch (Exception e) {
+                System.out.print(e.getMessage());
+            }
+            return null;
+        });
+    }
+
+    private void setupTable() {
+        TableColumn<Profissional, String> profissionalNome = createTableColumn("Nome", 100, "nome");
+        TableColumn<Profissional, String> profissionalTelefone = createTableColumn("Telefone", 100, "telefone");
+        TableColumn<Profissional, String> profissionalEspecialidade = createTableColumn("Especialidade", 100, "especialidade");
+        TableColumn<Profissional, Void> buttonColumn = createProfissionalButtonColumn();
+
+        TableViewProfissionais.getColumns().addAll(profissionalNome, profissionalTelefone, profissionalEspecialidade, buttonColumn);
+        TableViewProfissionais.setItems(profissionais);
+    }
 
     @FXML
     private void onClick(ActionEvent event) {
-        
+
     }
-    
+
 }
